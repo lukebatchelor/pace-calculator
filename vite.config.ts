bcc143055629b7ec5acc68cf3f97c089a635001d
Luke.Batchelor <batchelor.luke+git@gmail.com>
2022-12-29 10:43:50 +0800

    Adds vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,38 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vuetify({ autoImport: true })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vuetify({ autoImport: true }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Pace Calculator',
+        short_name: 'Pace Calculator',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: '.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
